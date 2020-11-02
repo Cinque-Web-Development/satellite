1f2bf1421a477cb6b05f5efe6c00ff4bb295e61e
Kathleen Stickel <kstick9210@outlook.com>
2020-11-02 10:18:58 -0600

    update base styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,5 +9,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
